beef659f060cddee358eebc0353920e8aee58bfe
HappyCthulhu5698 <kwila.holzknecht@education.nsw.gov.au>
2024-10-18 09:50:04 +1100

    shaboinked

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,31 +1,23 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 12f;
     private bool isFacingRight = true;
 
-    private bool doubleJump;
-
-    private bool isJumping;
+    private float doubleJump = 2;
 
     private float coyoteTime = 0.2f;
     private float coyoteTimeCounter;
 
-    private float jumpBufferTime = 0.2f;
+    private float jumpBufferTime = 1f;
     private float jumpBufferCounter;
 
-    private bool isWallSliding;
-    private float wallSlidingSpeed = 2f;
-
     private bool isWallJumping;
-    private float wallJumpingDirection;
-    private float wallJumpingTime = 0.2f;
-    private float wallJumpingCounter;
-    private float wallJumpingDuration = 0.4f;
-    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -40,35 +32,53 @@ public class PlayerMovement : MonoBehaviour
         if (IsGrounded())
         {
             coyoteTimeCounter = coyoteTime;
+            doubleJump = 2f;
         }
         else
         {
-            coyoteTimeCounter -= Time.deltaTime;
+            if (coyoteTimeCounter > 0)
+            {
+                coyoteTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                coyoteTimeCounter = 0;
+            }
         }
-
 
         if (Input.GetButtonDown("Jump"))
         {
-            if (coyoteTimeCounter >= 0f || doubleJump)
+            jumpBufferCounter = jumpBufferTime;
+            if (jumpBufferCounter > 0)
             {
-                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+                jumpBufferCounter -= Time.deltaTime;
+                if (coyoteTimeCounter > 0f || doubleJump > 0)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
 
-                doubleJump = !doubleJump;
+                    doubleJump -= 1;
+                }
+            }
+            else
+            {
+                jumpBufferCounter = 0;
             }
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.1f);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.1f);
         }
-
-        WallSlide();
-        WallJump();
-
-        if (!isWallJumping)
+        if (rb.velocity.y < 0f)
         {
-            Flip();
+            rb.gravityScale = Mathf.Lerp(rb.gravityScale, 6f, 20 * Time.deltaTime);
         }
+        else
+        {
+            rb.gravityScale = 4f;
+        }
+
     }
 
     private void FixedUpdate()
@@ -83,63 +93,6 @@ public class PlayerMovement : MonoBehaviour
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
-
-    private bool IsWalled()
-    {
-        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
-    }
-
-    private void WallSlide()
-    {
-        if (IsWalled() && !IsGrounded() && horizontal != 0f)
-        {
-            isWallSliding = true;
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
-        }
-        else
-        {
-            isWallSliding = false;
-        }
-    }
-
-    private void WallJump()
-    {
-        if (isWallSliding)
-        {
-            isWallJumping = false;
-            wallJumpingDirection = -transform.localScale.x;
-            wallJumpingCounter = wallJumpingTime;
-
-            CancelInvoke(nameof(StopWallJumping));
-        }
-        else
-        {
-            wallJumpingCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
-        {
-            isWallJumping = true;
-            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
-            wallJumpingCounter = 0f;
-
-            if (transform.localScale.x != wallJumpingDirection)
-            {
-                isFacingRight = !isFacingRight;
-                Vector3 localScale = transform.localScale;
-                localScale.x *= -1f;
-                transform.localScale = localScale;
-            }
-
-            Invoke(nameof(StopWallJumping), wallJumpingDuration);
-        }
-    }
-
-    private void StopWallJumping()
-    {
-        isWallJumping = false;
-    }
-
     private void Flip()
     {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
